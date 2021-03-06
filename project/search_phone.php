24db31cd68cebaf59fee33ac180c4aa0af29a819
Touya-code <79268815+Touya-code@users.noreply.github.com>
2021-03-07 01:38:00 +0900

    Update search_phone.php

--- project/search_phone.php
+++ project/search_phone.php
@@ -37,7 +37,7 @@ if(isset($_GET["maker"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Search SmartPhone</title>
     <link rel="stylesheet" href="stylesheet.css">
     <link rel="stylesheet" href="responsive.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
